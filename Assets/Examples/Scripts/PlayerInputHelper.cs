37c50e5e7b5fb7345e44aeb13ffcef7240f2ffae
blkcatman <tatsuro.matsubara@gmail.com>
2021-12-10 08:25:56 +0900

    射撃に対応する入力の導線を追加

--- Assets/Examples/Scripts/PlayerInputHelper.cs
+++ Assets/Examples/Scripts/PlayerInputHelper.cs
@@ -6,9 +6,18 @@ public class PlayerInputHelper : MonoBehaviour
     [SerializeField]
     private float stickDeadZone = 0.1f;
 
+    [SerializeField]
+    private float triggerDeadZone = 0.4f;
+
     public Vector2 Move { get; private set; }
+
+    public float Fire { get; private set; }
 
     public bool HasMoveInput => Move.magnitude > stickDeadZone;
 
+    public bool HasFireInput => Fire > triggerDeadZone;
+
     public void OnMove(InputValue value) => Move = value.Get<Vector2>();
+
+    public void OnFire(InputValue value) => Fire = value.Get<float>();
 }
